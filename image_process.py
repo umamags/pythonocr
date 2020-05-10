190706b2588afc81871a0250d12c25aa8cd1156f
Mahesh Natarajan <umamags@gmail.com>
2020-05-10 16:46:27 +0530

    works

--- image_process.py
+++ image_process.py
@@ -1,29 +1,33 @@
 # Improting Image class from PIL module 
 from PIL import Image 
 import sys 
+import tesseract
 
 def main(argv):
     # Opens a image in RGB mode 
-    outfile = "images\image_out.png"
-    outfile2 = "images\image_out_large.png"
-    im = Image.open(r"C:\workspace\python\images\image.png") 
+    topfile = "C:/workspace/python/pythonocr/images/find_the_error/top_image_out_"
+    bottomfile = "C:/workspace/python/pythonocr/images/find_the_error/bottom_image_out_"
+    im = Image.open(r"C:/workspace/python/pythonocr/images/find_the_error/image.png") 
         
     # Size of the image in pixels (size of orginal image) 
     # (This is not mandatory) 
     orig_width, orig_height = im.size 
     print(orig_width, orig_height)
     
-    new_size = int(orig_width*1.5), int(orig_height*1.5) 
     # Setting the points for cropped image 
     #left = 300
     #top = 80
     #right = 1000
     #bottom = 600
     #width = 415 -333 = 82
-    left = 333
-    top = 97
-    right = 333+82
-    bottom = 380
+    
+    # Top file
+    width = 161
+    height = 253    
+    left = 364
+    top = 113
+    right = left+width
+    bottom = top+height
     
     if len(argv) == 4:
         left = int(argv[0])
@@ -31,21 +35,42 @@ def main(argv):
         right = int(argv[2])
         bottom = int(argv[3])
     
-    print(left, top, right, bottom)
-    # Cropped image of above dimension 
-    # (It will not change orginal image) 
-    im1 = im.crop((left, top, right, bottom)) 
-    ch_width, ch_height = im1.size 
-    print(ch_width, ch_height)
+    for col in range(6):        
+        print(left, top, right, bottom)
+        im1 = im.crop((left, top, right, bottom)) 
+        ch_width, ch_height = im1.size 
+        im1.save(topfile + str(col) + ".png", 'png')
+        left = left + width
+        right = right + width
     
-    # Shows the image in image viewer 
-    im1.save(outfile, 'png')
-    im1.show()
+    # Bottom file
+    width = 100
+    height = 154    
+    left = 445
+    top = 408
+    right = left+width
+    bottom = top+height
+    for col in range(6):        
+        print(left, top, right, bottom)
+        im1 = im.crop((left, top, right, bottom)) 
+        
+        ch_width, ch_height = im1.size 
+        new_size = ch_width*2, ch_height*2
+        
+        im2 = im1.resize(new_size, Image.ANTIALIAS)
+        im2.save(bottomfile + str(col) + ".png", 'png')
+        left = left + width
+        right = right + width
     
-    im2 = im1.resize(new_size, Image.ANTIALIAS)
-    ch2_width, ch2_height = im2.size 
-    print(ch2_width, ch2_height)
-    im2.save(outfile2, 'png')
+    tesseract.process_images(topfile, bottomfile)    
+    #save_grayscale(infile, outfile)
+    #im2 = im1.resize(new_size, Image.ANTIALIAS)
+    #ch2_width, ch2_height = im2.size 
+    #print(ch2_width, ch2_height)
+    #im2.save(outfile2, 'png')
     
 if __name__ == "__main__":
+   print("Program started")
    main(sys.argv[1:])
+   print("Program ended")
+   
--- tesseract.py
+++ tesseract.py
@@ -17,13 +17,43 @@ def save_grayscale(infile, outfile):
     print("Writing " + outfile)
 # load the image as a PIL/Pillow image, apply OCR, and then delete
 # the temporary file
-def run_tesseract(filename):
-    text = pytesseract.image_to_string(Image.open(filename))
-    print(text)
+def process_images(top_image, bottom_image):
+    for col in range(6):   
+        top_image1 = top_image + str(col) + ".png"
+        bottom_image1 = bottom_image + str(col) + ".png"
+        run_tesseract(top_image1, bottom_image1)
+        
+def run_tesseract(top_image, bottom_image):
+    print(top_image)
+    top_text = pytesseract.image_to_string(Image.open(top_image))
+    bottom_text = pytesseract.image_to_string(Image.open(bottom_image))
+    compare(top_text, bottom_text)
     
+def compare(top_text, bottom_text):
+    top_array = []
+    list = top_text.split("\n")
+    for entry in list:
+        if len(entry) > 0:
+            top_array.append(entry)    
+    top_array = top_array[0:7]
+    print(top_array)
+    
+    bottom_array = []
+    list = bottom_text.split("\n")
+    for entry in list:
+        if len(entry) > 0:
+            bottom_array.append(entry)
+    print(bottom_array)
+    print("Difference::")
+    print(Diff(top_array,bottom_array))
+    
+
+def Diff(li1, li2): 
+    return (list(set(li1) - set(li2))) 
+       
 if __name__ == "__main__":
    #infile = "C:\workspace\python\images\image_out_large.png"
-   infile = "C:\workspace\python\images\image_out.png"
-   outfile = "C:\workspace\python\images\image_out_large_grey.png"
+   top_image = "C:/workspace/python/pythonocr/images/find_the_error/top_image_out_"
+   bottom_image = "C:/workspace/python/pythonocr/images/find_the_error/bottom_image_out_"
    #save_grayscale(infile, outfile)
-   run_tesseract(infile)
+   process_images(top_image, bottom_image)
