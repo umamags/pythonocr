b91aeb7ace77097bd8b546ee528116db65c742b4
Mahesh Natarajan <umamags@gmail.com>
2020-05-10 17:06:44 +0530

    latest code

--- image_process.py
+++ image_process.py
@@ -2,24 +2,24 @@
 from PIL import Image 
 import sys 
 import tesseract
+import pandas as pd 
+import xlrd 
 
 def main(argv):
-    # Opens a image in RGB mode 
-    topfile = "C:/workspace/python/pythonocr/images/find_the_error/top_image_out_"
-    bottomfile = "C:/workspace/python/pythonocr/images/find_the_error/bottom_image_out_"
-    im = Image.open(r"C:/workspace/python/pythonocr/images/find_the_error/image.png") 
-        
-    # Size of the image in pixels (size of orginal image) 
-    # (This is not mandatory) 
-    orig_width, orig_height = im.size 
-    print(orig_width, orig_height)
+    basedir = "C:/workspace/python/pythonocr"
+    excel_mapping_file = basedir + "/mappings/image_mappings.xlsx"
+     
+    df0 = pd.read_excel(excel_mapping_file, sheet_name="Main")
+    print(df0.shape)
     
-    # Setting the points for cropped image 
-    #left = 300
-    #top = 80
-    #right = 1000
-    #bottom = 600
-    #width = 415 -333 = 82
+    foldername = "find_the_error"
+    if len(argv) > 0:
+        foldername = argv[0]
+    topfile = basedir + "/images/" + foldername + "/top_image_out_"
+    bottomfile = basedir + "/images/" + foldername + "/bottom_image_out_"
+    im = Image.open(r"" + basedir + "/images/" + foldername + "/image.png") 
+        
+    orig_width, orig_height = im.size 
     
     # Top file
     width = 161
@@ -29,14 +29,7 @@ def main(argv):
     right = left+width
     bottom = top+height
     
-    if len(argv) == 4:
-        left = int(argv[0])
-        top = int(argv[1])
-        right = int(argv[2])
-        bottom = int(argv[3])
-    
     for col in range(6):        
-        print(left, top, right, bottom)
         im1 = im.crop((left, top, right, bottom)) 
         ch_width, ch_height = im1.size 
         im1.save(topfile + str(col) + ".png", 'png')
